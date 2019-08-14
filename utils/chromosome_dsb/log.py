40f5c340f898b63580eaba9c11aff436e4833abe
cespenel <cedric.espenel@gmail.com>
2019-08-14 15:48:00 -0700

    Adjustement log file

--- utils/chromosome_dsb/log.py
+++ utils/chromosome_dsb/log.py
@@ -1,9 +1,8 @@
 import os
 
 def log_file(directory, meta, **kwargs):
-    back_sub_ch1 = kwargs['back_sub_ch1']
-    back_sub_ch2 = kwargs['back_sub_ch2']
-    back_sub_ch3 = kwargs['back_sub_ch3']
+    back_sub_FOC = kwargs['back_sub_ch1']
+    back_sub_Nucleus = kwargs['back_sub_ch2']
     small_object = kwargs['small_object']
     large_object = kwargs['large_object']
     threshold = kwargs['threshold']
@@ -11,10 +10,9 @@ def log_file(directory, meta, **kwargs):
     with open(directory+'/'+meta["Name"]+".txt",'w') as file:
         file.write('Size kernel ch1 : {}\n'\
                    'Size kernel ch2 : {}\n'\
-                   'Size kernel ch3 : {}\n'\
                    'Size of the smallest Sigma : {}\n'\
                    'Size of the largest Sigma : {}\n'\
                    'Threshold: {}\n'\
-                    .format(str(back_sub_ch1),str(back_sub_ch2),
-                            str(back_sub_ch3), str(small_object),
+                    .format(str(back_sub_FOC),str(back_sub_Nucleus),
+                            str(small_object),
                             str(large_object), str(threshold)))
